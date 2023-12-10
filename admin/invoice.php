728711c3084b8791c2d49ee8c307dadb4f0154f2
MarcusLuu <85067941+LuuHuynhVinhPhuc@users.noreply.github.com>
2023-12-10 23:34:51 +0700

    Get a first commit

---
+++ admin/invoice.php
@@ -0,0 +1,125 @@
+<?php 
+  require ("../fpdf186/fpdf.php");
+
+  //customer and invoice details
+  $info=[
+    "customer"=>"Marcus",
+    "invoice_no"=>"0LG01739T3241472T",
+    "invoice_date"=>"Nov22,2023",
+    "total_amt"=>"200.00",
+  ];
+  
+  
+  //invoice Products
+  $products_info=[
+    [
+      "name"=>"123",
+      "price"=>"100",
+      "qty"=>3,
+      "total"=>"300"
+    ]
+  ];
+  
+  class PDF extends FPDF
+  {
+    function Header(){
+      
+      //Display Company Info
+      $this->SetFont('Arial','B',14);
+      $this->Cell(50,10,"Maxican Burger",0,1);
+      $this->SetFont('Arial','',14);
+      $this->Cell(50,7,"West Street,",0,1);
+      $this->Cell(50,7,"Salem 636002.",0,1);
+      $this->Cell(50,7,"PH : 0123456789",0,1);
+      
+      //Display INVOICE text
+      $this->SetY(15);
+      $this->SetX(-40);
+      $this->SetFont('Arial','B',18);
+      $this->Cell(50,10,"INVOICE",0,1);
+      
+      //Display Horizontal line
+      $this->Line(0,48,210,48);
+    }
+    
+    function body($info,$products_info){
+      
+      //Billing Details
+      $this->SetY(55);
+      $this->SetX(10);
+      $this->SetFont('Arial','B',12);
+      $this->Cell(50,10,"Bill To: ",0,1);
+      $this->SetFont('Arial','',12);
+      $this->Cell(50,7,$info["customer"],0,1);
+      
+      //Display Invoice no
+      $this->SetY(55);
+      $this->SetX(60);
+      $this->Cell(40,8,"Invoice No: ".$info["invoice_no"]);
+      
+      //Display Invoice date
+      $this->SetY(63);
+      $this->SetX(60);
+      $this->Cell(40,8,"Invoice Date: ".$info["invoice_date"]);
+      
+      //Display Table headings
+      $this->SetY(95);
+      $this->SetX(10);
+      $this->SetFont('Arial','B',12);
+      $this->Cell(80,9,"DESCRIPTION",1,0);
+      $this->Cell(40,9,"PRICE",1,0,"C");
+      $this->Cell(30,9,"QTY",1,0,"C");
+      $this->Cell(40,9,"TOTAL",1,1,"C");
+      $this->SetFont('Arial','',12);
+      
+      //Display table product rows
+      foreach($products_info as $row){
+        $this->Cell(80,9,$row["name"],"LR",0);
+        $this->Cell(40,9,$row["price"],"R",0,"R");
+        $this->Cell(30,9,$row["qty"],"R",0,"C");
+        $this->Cell(40,9,$row["total"],"R",1,"R");
+      }
+      //Display table empty rows
+      for($i=0;$i<12-count($products_info);$i++)
+      {
+        $this->Cell(80,9,"","LR",0);
+        $this->Cell(40,9,"","R",0,"R");
+        $this->Cell(30,9,"","R",0,"C");
+        $this->Cell(40,9,"","R",1,"R");
+      }
+      //Display table total row
+      $this->SetFont('Arial','B',12);
+      $this->Cell(150,9,"TOTAL",1,0,"R");
+      $this->Cell(40,9,$info["total_amt"],1,1,"R");
+      
+      //Display amount in words
+      $this->SetY(225);
+      $this->SetX(10);
+      $this->SetFont('Arial','B',12);
+      $this->Cell(0,9,"Amount in Words ",0,1);
+      $this->SetFont('Arial','',12);
+      
+    }
+    function Footer(){
+      
+      //set footer position
+      $this->SetY(-50);
+      $this->SetFont('Arial','B',12);
+      $this->Cell(0,10,"for Maxiccan Burger",0,1,"R");
+      $this->Ln(15);
+      $this->SetFont('Arial','',12);
+      $this->Cell(0,10,"Authorized Signature",0,1,"R");
+      $this->SetFont('Arial','',10);
+      
+      //Display Footer Text
+      $this->Cell(0,10,"This is a computer generated invoice",0,1,"C");
+      
+    }
+    
+  }
+  //Create A4 Page with Portrait 
+  $pdf=new PDF("P","mm","A4");
+  $pdf->AddPage();
+  $pdf->body($info,$products_info);
+  $pdf->Output();
+?>
